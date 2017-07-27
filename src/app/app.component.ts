a7ea2a1c32b402fd0f784d223c3c08c907274748
guanzhanliang <19879753@qq.com>
2017-07-27 16:49:45 +0800

    创建用户和密码输入框

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
+interface User {
+  username: string;
+  password: string;
+  age?: number;
+  sex?: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // title = 'app';
+  user: any = {};
+  user1: User = {
+    username: 'guanzhanliang',
+    password: '123123',
+    age: 30,
+    sex: '1',
+  }
+
+  onKeyPressOnlyNumber(event: any) {
+    let keyCode = event.keyCode;
+    if ((keyCode >= 48 && keyCode <= 57) || (keyCode >= 8 && keyCode <= 13) || (keyCode >= 96 && keyCode <= 105)) {
+      event.returnValue = true;
+    } else {
+      event.returnValue = false;
+    }
+  }
 }
